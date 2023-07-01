572e3070ca2a2e0e88524b4d4209151e76be4577
Andrii Kurshatsov <andreswebit@ukr.net>
2023-07-01 12:23:33 +0300

    Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+//Створить окремий елемент <li>. Обов'язково використати метод document.createElement().
+//Додати назву інгредієнта як його текстовий вміст.
+//Додати елементу клас item.
+
+const itemsIngredients = ingredients.map((ingredient) => {
+  const ItemIngredient = document.createElement("li");
+  ItemIngredient.classList.add("item");
+  ItemIngredient.textContent = ingredient;
+  return ItemIngredient;
+});
+
+//Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+const listUlIngridients = document.querySelector("ul#ingredients");
+listUlIngridients.append(...itemsIngredients);
